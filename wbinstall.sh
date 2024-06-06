6e3f32447c0cde42b369532170baae987ed10240
Wallace Bruno <38006634+chwiee@users.noreply.github.com>
2024-06-06 16:34:16 -0300

    Update wbinstall.sh

--- wbinstall.sh
+++ wbinstall.sh
@@ -23,12 +23,14 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Custom Ubuntu with i3
-printf "%-80s" "Apt repository regolith"
-sudo add-apt-repository ppa:regolith-linux/release
-check $?
-
-printf "%-80s" "Install regolith desktop"
-sudo apt install regolith-desktop
+printf "%-80s" "Apt repository regolith and install "
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-3_1-ubuntu-mantic-amd64 mantic main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+sudo apt update
+sudo apt install regolith-desktop regolith-session-flashback regolith-look-i3-default
 check $?
 
 printf "%-80s" "Apt repository regolith i3 bar"
@@ -40,28 +42,11 @@ sudo apt install i3xrocks-battery
 check $?
 
 # General Settings
-if [ ! -d /projects ]; then
-  printf "%-80s" "Creating /projects folder "
-  mkdir /projects
-  check $?
-fi
-
-if [ ! -d /projects/scripts ]; then
-  printf "%-80s" "Copy ./script folder to /projects"
-  cp ./scripts /projects/scripts
-fi
-
 if [ ! -d ~/.config/nvim ]; then
   printf "%-80s" "Setting nvim config"
   mkdir ~/.config/nvim &>/dev/null
   check  $?
 fi
-
-printf "%-80s" "Configuring REGOLITH I3 config"
-yes | cp ./configs/i3 /etc/regolith/i3/config &>/dev/null
-ln -s /etc/regolith/i3/config ~/.config/i3
-check $?
-
 printf "%-80s" "Configuring zshrc config"
 yes | cp ./configs/zshrc ~/.zshrc &>/dev/null
 check $?
@@ -102,7 +87,6 @@ declare -a HUE=(
   poppler-utils 
   mediainfo 
   arandr 
-  firefox 
   zsh 
   cmatrix 
   xclip 
@@ -114,28 +98,8 @@ for BR in ${HUE[@]}; do
   wbinstall $BR 
 done
 
-# Need to Devs
-declare -a DEVS=(
-  python-software-properties 
-  fonts-firacode 
-  nodejs 
-  npm 
-  yarn 
-  python-pip 
-  python3 
-  clang 
-  gcc 
-  make 
-)
-
-for DEV in ${DEVS[@]}; do
-  wbinstall $DEV
-done
-
-# Need to DevOps
 declare -a OPS=(
   git 
-  docker 
   screen 
   curl 
   wget 
@@ -148,64 +112,3 @@ for OP in ${OPS[@]}; do
   wbinstall $OP
 done
 
-
-printf "%-80s" "Installing Go Lang"
-  cd /tmp
-  wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz 
-  tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
-check $?
-
-printf "%-80s" "Installing Helm 3"
-  cd /tmp
-  wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz
-  tar -xvzf helm-v3.2.4-linux-amd64.tar.gz
-  chmod +x linux-amd64/helm
-  sudo cp linux-amd64/helm /usr/local/bin/helm3
-  sudo ln -sfn /usr/local/bin/helm3 /usr/local/bin/helm
-check $?
-
-printf "%-80s" "Installing helm-docs"
-  cd /tmp
-  git clone https://github.com/norwoodj/helm-docs
-  cd helm-docs/cmd/helm-docs
-  go build
-  mv helm-docs /usr/local/bin/
-  chmod +rx /usr/local/bin/helm-docs
-check $?
-
-printf "%-80s" "Installing Kubernetes - kubectl"
-  cd /tmp &>/dev/null
-  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" &>/dev/null
-  chmod +x ./kubectl &>/dev/null
-  sudo mv ./kubectl /usr/local/bin/kubectl &>/dev/null
-check $?
-
-printf "%-80s" "Installing eksctl"
-  curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp &>/dev/null
-  mv /tmp/eksctl /usr/local/bin &>/dev/null
-check $?
-
-printf "%-80s" "Installing Kubectx"
-  cd /tmp
-  wget https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
-  chmod +x kubectx
-  mv kubectx /usr/local/bin
-check $?
-
-printf "%-80s" "Installing Kubens"
-  cd /tmp
-  wget https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens 
-  chmod +x  kubens
-  sudo mv kubens  /usr/local/bin
-check $?
-
-printf "%-80s" "Export PATH"
-  export PATH=$PATH:/usr/local/go/bin
-  export PATH=$PATH:/usr/local/bin/helm
-  export PATH=$PATH:/usr/local/bin/helm3
-  export PATH=$PATH:/usr/local/bin/helm-docs
-  export PATH=$PATH:/usr/local/bin/kubectl
-  export PATH=$PATH:/usr/local/bin/eksctl
-  export PATH=$PATH:/usr/local/bin/kubectx
-  export PATH=$PATH:/usr/local/bin/kubens
-check $?
